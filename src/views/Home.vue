1565bf2ef9e2645a2bbbefe22c1163510faeda22
Jack Kinsey <jack.w.kinsey@gmail.com>
2020-01-28 23:09:37 -0500

    feat: add Profile page to show user info
    When user is logged in they can view their user info at the
    profile route.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,31 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+
+    <div v-if="!$auth.loading">
+      <button v-if="!$auth.isAuthenticated" @click="login">Log in</button>
+      <button v-if="$auth.isAuthenticated" @click="logout">Log out</button>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'home',
   components: {
     HelloWorld,
+  },
+  methods: {
+    login() {
+      this.$auth.loginWithRedirect();
+    },
+    logout() {
+      this.$auth.logout({
+        returnTo: window.location.origin,
+      });
+    },
   },
 };
 </script>
---
+++ src/views/Profile.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div>
+      <img :src="$auth.user.picture" :alt="`image of ${$auth.user.name}`" />
+      <div>{{ $auth.user.name }}</div>
+      <div>{{ $auth.user.email }}</div>
+    </div>
+
+    <div>
+      <pre>{{ JSON.stringify($auth.user, null, 2) }}</pre>
+    </div>
+  </div>
+</template>
